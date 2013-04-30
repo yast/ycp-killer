632abfbf71dc3d749a18f27d4867b33202f3ceb7
Josef Reidinger <jreidinger@suse.cz>
2013-04-30 08:21:13 +0000

    add build command

---
+++ lib/commands/build_command.rb
@@ -0,0 +1,13 @@
+require_relative "command"
+
+module Commands
+  class BuildCommand < Command
+    def apply(mod)
+      action "Building" do
+        Dir.chdir mod.obs_dir do
+          Cheetah.run "osc", "build", "openSUSE_12.3"
+        end
+      end
+    end
+  end
+end
