ad6f5bee888a1afc00fc3d7e70626acedfbcc30e
David Majda <dmajda@suse.cz>
2013-05-21 14:46:32 +0200

    Stub MasterCFParser.pm
    Stubbed because otherwise the "mail" YaST module will attempt to load
    this file from the system, where it is broken.

---
+++ stubs/MasterCFParser.pm
@@ -0,0 +1,9 @@
+# Stubbed because otherwise the "mail" YaST module will attempt to load this
+# file from the system, where it is broken:
+#
+#   $ perl -c /usr/lib/YaST2/servers_non_y2/MasterCFParser.pm
+#   syntax error at /usr/lib/YaST2/servers_non_y2/MasterCFParser.pm line 406, near "{command :"
+#   Missing right curly or square bracket at /usr/lib/YaST2/servers_non_y2/MasterCFParser.pm line 499, at end of line
+#   /usr/lib/YaST2/servers_non_y2/MasterCFParser.pm had compilation errors.
+
+1;
