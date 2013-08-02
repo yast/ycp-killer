f4638ce450a3bbefb52653310dc967a48d96acec
David Majda <dmajda@suse.cz>
2013-08-02 15:02:30 +0200

    Add --ref, --branch and --tag options to "yk clone"
    The --branch and --tag options are equal to --ref, they are just
    provided for readability (this is inspired by Bundler).
    
    These options will allow YCP Killer to be used even after the final
    conversion to Ruby was done in master. This might be useful e.g. for
    porting SLE fixes (which must be done in YCP) to master.

--- lib/commands/clone_command.rb
+++ lib/commands/clone_command.rb
@@ -4,11 +4,15 @@ require_relative "command"
 
 module Commands
   class CloneCommand < Command
+    def initialize(options = {})
+      @ref = options[:ref] || options[:branch] || options[:tag] || "master"
+    end
+
     def apply(mod)
       action "Cloning" do
         FileUtils.rm_rf(mod.work_dir)
 
-        Cheetah.run "git", "clone", "git@github.com:yast/yast-#{mod.name}.git", mod.work_dir
+        Cheetah.run "git", "clone", "git@github.com:yast/yast-#{mod.name}.git", "--branch", @ref, mod.work_dir
       end
     end
   end
